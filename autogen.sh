65a9f2a5b562619ddf80af278c7d3a5c2892b00d
Darryl Sokoloski <dsokoloski@clearfoundation.com>
2014-03-12 17:31:57 -0400

    Updated autoconf, removed obsolete/generated files.

--- autogen.sh
+++ autogen.sh
@@ -3,9 +3,6 @@
 find $(pwd) -name configure.ac | xargs touch
 
 # Regenerate configuration files
-aclocal || exit 1
-automake --foreign --include-deps --add-missing --copy || exit 1
-autoreconf -i -f || exit 1
+mkdir -vp m4
+autoreconf -i --force -I m4 || exit 1
 
-# Run configure for this platform
-./configure $*
